10e870e4d4edab600a8ec063d2935c0c0f8e7890
Ilya <example@gmail.com>
2023-10-23 11:28:36 +0300

    first commit

---
+++ 1lesson/js/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+
+let incr = 10,
+    decr = 10;
+
+
+// ++incr;
+// --decr;
+console.log(incr++, decr--)
+
+console.log(5%4)
+
+console.log(2*4 != 8)
+
+let checked = true,
+    close = false;
+
+console.log(checked && !close);
+
